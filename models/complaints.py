833c9617e9631f3a698ced527b957c9649e8356b
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2023-11-01 21:09:38 +0200

    adad

---
+++ models/complaints.py
@@ -0,0 +1,15 @@
+from sqlalchemy import UUID, ForeignKey, String, Boolean, Column, TIMESTAMP
+from database import Base
+from models.organizations import Organizations
+from models.users import Users
+import uuid
+import datetime
+
+class Workers(Base):
+    __tablename__ = "workers"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID,ForeignKey("Users.id"),nullable=False)
+    org_id = Column(UUID,ForeignKey("Organizations.id"),nullable=False)
+    body = Column(String,nullable=False)
+    created_at = Column(TIMESTAMP,default=datetime.utcnow)
