727847e631407a155247130bda58d690cd8c1c20
banrj <eh.ehr@bk.ru>
2023-11-01 21:38:44 +0200

    test

--- models/offers.py
+++ models/offers.py
@@ -5,11 +5,12 @@ from models.users import Users
 import uuid
 import datetime
 
+
 class Offers(Base):
     __tablename__ = "offers"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID,ForeignKey("Users.id"),nullable=False)
-    org_id = Column(UUID,ForeignKey("Organizations.id"),nullable=False)
-    body = Column(String,nullable=False)
-    created_at = Column(TIMESTAMP,default=datetime.utcnow)
+    user_id = Column(UUID, ForeignKey("Users.id"), nullable=False)
+    org_id = Column(UUID, ForeignKey("Organizations.id"), nullable=False)
+    body = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
